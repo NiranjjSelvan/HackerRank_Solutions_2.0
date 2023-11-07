b43f2908762fdd5c914eacd3e9489a75d951f5dc
Niranjj Selvan <sniranjj@gmail.com>
2023-11-07 13:08:41 +0530

    Copied logic

--- src/Strings/Tokens_RegEx.java
+++ src/Strings/Tokens_RegEx.java
@@ -6,14 +6,31 @@ public class Tokens_RegEx {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String input = scan.nextLine();
+		String s = scan.nextLine();
 		scan.close();
-		String regex = "[A-Za-z]+";
-		String[] tokens = input.split(regex);
-		System.out.println(tokens.length);
-		for (String token : tokens) {
-			System.out.println(token);
+
+		s = removeLeadingNonLetters(s);
+
+		if (s.length() == 0) {
+			System.out.println(0);
+			return;
+		}
+
+		String[] words = s.split("[^a-zA-Z]+");
+
+		System.out.println(words.length);
+		for (String word : words) {
+			System.out.println(word);
 		}
 	}
 
+	private static String removeLeadingNonLetters(String str) {
+		int i;
+		for (i = 0; i < str.length(); i++) {
+			if (Character.isLetter(str.charAt(i))) {
+				break;
+			}
+		}
+		return str.substring(i);
+	}
 }
